c5d6f7bdb033646b81204ce4b35d68e1d65c0110
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2024-11-18 19:32:20 +0100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    packages.${system} = rec {
+      sshrm = pkgs.callPackage ./pkgs/sshrm {};
+      ### Add new pkgs here
+    };
+  };
+}
