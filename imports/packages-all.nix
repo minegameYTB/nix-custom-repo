10ebfdf18862d45b5c7b9978d86393fe9eb208e9
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2024-11-23 03:49:00 +0100

    pkgs/misc: add misc categorie

--- imports/packages-all.nix
+++ imports/packages-all.nix
@@ -6,6 +6,7 @@
     {
       packages = {
         sshrm = pkgs.callPackage ../pkgs/cli/sshrm {};
+        hello = pkgs.callPackage ../pkgs/misc/hello {};
         ### Add new pkgs here
       };
     };
---
+++ pkgs/misc/hello/default.nix
@@ -0,0 +1,11 @@
+{ stdenv, fetchzip }:
+
+ stdenv.mkDerivation rec {
+   pname = "hello";
+   version = "2.12.1";
+
+   src = fetchzip {
+   	 url = "https://ftp.gnu.org/gnu/${pname}/${pname}-${version}.tar.gz";
+   	 sha256 = "sha256-1kJjhtlsAkpNB7f6tZEs+dbKd8z7KoNHyDHEJ0tmhnc=";
+   };
+ }
