1d0d6f6c6b6f01083c13abaa75f006dbae872d98
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2024-11-25 16:58:06 +0100

    Revert "try to add webapp-manager"
    This reverts commit bef84fa26e75de3de6d278e358f3fdd4e6d4f2e0.

--- imports/packages-all.nix
+++ imports/packages-all.nix
@@ -7,7 +7,6 @@
       packages = {
         sshrm = pkgs.callPackage ../pkgs/cli/sshrm {};
         hello = pkgs.callPackage ../pkgs/misc/hello {};
-        webapp-manager = pkgs.callPackage ../pkgs/applications/web/webapp-manager {};
         ### Add new pkgs here
       };
     };
