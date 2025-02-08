d48be472020b03fcfa391ba5f0ea54f24ea04716
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-02-08 01:00:26 +0100

    Update packages-all.nix

--- imports/packages-all.nix
+++ imports/packages-all.nix
@@ -6,7 +6,7 @@
     {
       packages = {
         sshrm = pkgs.callPackage ../pkgs/cli/sshrm {};
-        fhsEnv-dev = pkgs.callPackage ../pkgs/cli/fhsEnv-dev {};
+        fhsEnv = pkgs.callPackage ../pkgs/cli/fhsEnv-dev {};
         hello = pkgs.callPackage ../pkgs/misc/hello {};
         ### Add new pkgs here
       };
