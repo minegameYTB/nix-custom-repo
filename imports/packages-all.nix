6f1247ccfe33685d80f53b41e35b637739f330ae
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-02-08 01:05:57 +0100

    Revert "Update packages-all.nix"
    This reverts commit d48be472020b03fcfa391ba5f0ea54f24ea04716.

--- imports/packages-all.nix
+++ imports/packages-all.nix
@@ -6,7 +6,7 @@
     {
       packages = {
         sshrm = pkgs.callPackage ../pkgs/cli/sshrm {};
-        fhsEnv = pkgs.callPackage ../pkgs/cli/fhsEnv-dev {};
+        fhsEnv-dev = pkgs.callPackage ../pkgs/cli/fhsEnv-dev {};
         hello = pkgs.callPackage ../pkgs/misc/hello {};
         ### Add new pkgs here
       };
