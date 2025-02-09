2f4a8525f7ac2e6a5ea6a164db0437519469f64a
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-02-09 04:15:20 +0100

    GLFfetch git: init at git-7322499 (fork by me)

---
+++ pkgs/cli/GLF-Fetch/default.nix
@@ -0,0 +1,41 @@
+{ stdenvNoCC, substituteAll, writeScriptBin, fetchFromGitHub, fastfetch, coreutils, gawk, makeWrapper }:
+
+stdenvNoCC.mkDerivation rec {
+  pname = "GLFfetch-nixos";
+  version = "git-${builtins.substring 0 7 src.rev}";  # Dynamically generate version from Git hash
+
+  src = fetchFromGitHub {
+    owner = "minegameYTB";
+    repo = pname;
+    rev = "73224999309bbfe0b356507672621d138ff8deff";
+    sha256 = "sha256-gbd/boOHSqK8EHD+7y1k3bAxGwsHnkXJGgzKYgBhi3A=";
+  };
+
+  outputs = [ "out" "assets" ];
+  outputsToInstall = outputs;
+
+  buildInputs = [ fastfetch coreutils ];
+  nativeBuildInputs = [ makeWrapper ];
+
+  installPhase = ''
+    mkdir -p $out/bin $assets/share/${pname} $assets/share/${pname}/scripts
+    
+    cp $src/challenge.jsonc $assets/share/${pname}/challenge.jsonc
+    cp $src/GLF.png $assets/share/${pname}/GLF.png
+    cp -r $src/scripts/* $assets/share/${pname}/scripts/
+
+    substituteInPlace $assets/share/${pname}/challenge.jsonc \
+      --replace @GLF-path@ $assets/share/${pname}
+    
+    for script in $assets/share/${pname}/scripts/*.sh; do
+      substituteInPlace "$script" --replace @GLF-path@ $assets/share/${pname} \
+        --replace @coreutils@ ${coreutils} \
+        --replace @gawk@ ${gawk}
+      chmod +x "$script"
+    done
+
+    makeWrapper ${fastfetch}/bin/fastfetch $out/bin/GLFfetch \
+      --add-flags "--config $assets/share/${pname}/challenge.jsonc --chafa $assets/share/${pname}/GLF.png" \
+      --prefix PATH : ${coreutils}/bin
+  '';
+}
