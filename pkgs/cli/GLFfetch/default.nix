5d9e7f80cbeee5d6d84d00cfb05faee6686970e5
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-02-10 19:11:28 +0100

    change option of derivation to add GLFos icon

--- pkgs/cli/GLFfetch/default.nix
+++ pkgs/cli/GLFfetch/default.nix
@@ -1,14 +1,25 @@
-{ stdenvNoCC, substituteAll, writeScriptBin, fetchFromGitHub, fastfetch, coreutils, gawk, bash, makeWrapper }:
+{ 
+  stdenvNoCC, 
+  substituteAll, 
+  writeScriptBin, 
+  fetchFromGitHub, 
+  makeWrapper, 
+  fastfetch, 
+  coreutils, 
+  gawk,
+  bash, 
+  glfIcon ? "GLF"  # Paramètre pour choisir l'icône ("GLF" ou "GLFos")
+}:
 
 stdenvNoCC.mkDerivation rec {
   pname = "GLFfetch-nixos";
-  version = "git-${builtins.substring 0 7 src.rev}";  ### To update version number
+  version = "git-${builtins.substring 0 7 src.rev}";
 
   src = fetchFromGitHub {
     owner = "minegameYTB";
     repo = pname;
-    rev = "41632799a15c9c9497f5cdb80ed9fabc6dd8996f";
-    sha256 = "sha256-FVW9/p1U/fe8QFpJUust5cc14hIjfUKIqrAFpmHMBec=";
+    rev = "a0935f03d32acdeb108798f3fe9cfb18ce5413a1";
+    sha256 = "sha256-fO+vko4Ef41jXReIhZv2BVPTkETpAslZUrdBhyvVO2w=";
   };
 
   outputs = [ "out" "assets" ];
@@ -18,18 +29,33 @@ stdenvNoCC.mkDerivation rec {
   nativeBuildInputs = [ makeWrapper ];
 
   installPhase = ''
-    mkdir -p $out/bin $assets/share/${pname} $assets/share/${pname}/scripts
-    
-    cp $src/challenge.jsonc $assets/share/${pname}/challenge.jsonc
+    mkdir -p $out/bin $assets/share/${pname}/scripts
+
+    # Copier les icônes
     cp $src/GLF.png $assets/share/${pname}/GLF.png
-    
+    cp $src/GLFos.png $assets/share/${pname}/GLFos.png
+
+    # Sélectionner l'icône à utiliser
+    iconPath=$assets/share/${pname}/${glfIcon}.png
+
+    if [ ! -f "$iconPath" ]; then
+      echo "Warning: this icon does not exist, Use GLF or GLFos instead"
+      iconPath=$assets/share/${pname}/GLF.png
+    fi
+
+    # Copier le fichier de configuration
+    cp $src/challenge.jsonc $assets/share/${pname}/challenge.jsonc
+
+    # Modifier le fichier de configuration avec l'icône choisie
+    substituteInPlace $assets/share/${pname}/challenge.jsonc \
+      --replace-warn @GLF-path@ "$assets/share/${pname}" \
+      --replace-warn @GLFos-icon@ "$iconPath" \
+      --replace-warn @shell@ "${bash}/bin/bash"
+
+    # Copier les scripts
     if [ -d "$src/scripts" ]; then
       cp -r $src/scripts/* $assets/share/${pname}/scripts/
     fi
-
-    substituteInPlace $assets/share/${pname}/challenge.jsonc \
-      --replace-warn @GLF-path@ "$assets/share/${pname}" \
-      --replace-warn @shell@ "${bash}/bin/bash"
 
     for script in $assets/share/${pname}/scripts/*.sh; do
       substituteInPlace "$script" \
@@ -46,4 +72,3 @@ stdenvNoCC.mkDerivation rec {
     ln -s $out/bin/GLFfetch $out/bin/GLFfetch-nixos
   '';
 }
-
