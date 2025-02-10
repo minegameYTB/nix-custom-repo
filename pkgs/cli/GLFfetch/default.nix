8a5ccbe529017d61331c14f0f653620679f534a6
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-02-10 19:44:03 +0100

    GLFfetch: remove unused test on script

--- pkgs/cli/GLFfetch/default.nix
+++ pkgs/cli/GLFfetch/default.nix
@@ -8,7 +8,7 @@
   coreutils, 
   gawk,
   bash, 
-  glfIcon ? "GLF"  # Paramètre pour choisir l'icône ("GLF" ou "GLFos")
+  glfIcon ? "GLF"  # ### Use GLF icon or GLFos icon (to change icon)
 }:
 
 stdenvNoCC.mkDerivation rec {
@@ -29,30 +29,21 @@ stdenvNoCC.mkDerivation rec {
   nativeBuildInputs = [ makeWrapper ];
 
   installPhase = ''
+    ### Create directory
     mkdir -p $out/bin $assets/share/${pname}/scripts
-
-    # Copier les icônes
     cp $src/GLF.png $assets/share/${pname}/GLF.png
     cp $src/GLFos.png $assets/share/${pname}/GLFos.png
 
-    # Sélectionner l'icône à utiliser
+    # Set icon on a variable
     iconPath=$assets/share/${pname}/${glfIcon}.png
 
-    if [ ! -f "$iconPath" ]; then
-      echo "Warning: this icon does not exist, Use GLF or GLFos instead"
-      iconPath=$assets/share/${pname}/GLF.png
-    fi
-
-    # Copier le fichier de configuration
     cp $src/challenge.jsonc $assets/share/${pname}/challenge.jsonc
 
-    # Modifier le fichier de configuration avec l'icône choisie
     substituteInPlace $assets/share/${pname}/challenge.jsonc \
       --replace-warn @GLF-path@ "$assets/share/${pname}" \
       --replace-warn @GLFos-icon@ "$iconPath" \
       --replace-warn @shell@ "${bash}/bin/bash"
 
-    # Copier les scripts
     if [ -d "$src/scripts" ]; then
       cp -r $src/scripts/* $assets/share/${pname}/scripts/
     fi
