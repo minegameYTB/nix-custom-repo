e158bbc70bbe14660a1f139c02dc5384a5f6c6f4
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-02-10 19:16:12 +0100

    Add GLFfetch-GLFos attribute for GLFfetch package

--- imports/packages-all.nix
+++ imports/packages-all.nix
@@ -8,6 +8,7 @@
         sshrm = pkgs.callPackage ../pkgs/cli/sshrm {};
         fhsEnv-dev = pkgs.callPackage ../pkgs/cli/fhsEnv-dev {};
         GLFfetch = pkgs.callPackage ../pkgs/cli/GLFfetch {};
+        GLFfetch-GLFos = pkgs.callPackage ../pkgs/cli/GLFfetch { glfIcon = "GLFos"; };
         hello = pkgs.callPackage ../pkgs/misc/hello {};
         ### Add new pkgs here
       };
