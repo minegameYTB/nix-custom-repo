a8960cd3fdb2c71d154deacb5c1ce3e7b67184a9
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-02-22 12:06:32 +0100

    sshrm: reduce complexity of sshUtilsOnly.nix

--- pkgs/cli/sshrm/default.nix
+++ pkgs/cli/sshrm/default.nix
@@ -1,9 +1,14 @@
-{ lib, stdenvNoCC, openssh, makeWrapper, fetchFromGitHub }:
+{ lib, stdenvNoCC, openssh, makeWrapper, fetchFromGitHub, callPackage }:
+
+let
+  ### Import sshUtilsOnly derivation
+  sshUtilsOnly = callPackage ./deps/sshUtilsOnly.nix {};
+in
 
  stdenvNoCC.mkDerivation rec {
    repoName = "sshrm";
    pname = "sshrm";
-   version = "git-0803f98";
+   version = "git-${builtins.substring 0 7 src.rev}"; ### Update dynamically the version number (based on git commit version)
 
   src = fetchFromGitHub {
     owner = "aaaaadrien";
@@ -13,27 +18,22 @@
   };
 
   outputs = [ "out" "doc" ];
-  buildInputs = [ openssh makeWrapper ];
+  outputsToInstall = outputs;
+  buildInputs = [ sshUtilsOnly makeWrapper ];
 
   installPhase = ''
-    ### Make sshrm available to nix
-    mkdir -p $out/bin
+    ### Make sshrm available
+    mkdir -p $out/bin $doc/share/doc/${pname}
     cp ${pname} $out/bin/${pname}
-    chmod +x $out/bin/${pname}
-    wrapProgram $out/bin/${pname} \
-      --prefix PATH : ${lib.makeBinPath [openssh]}
 
-    ### Add license file accessible on the right directory
-    mkdir -p $doc/share/doc/sshrm
-    cp LICENSE $doc/share/doc/sshrm/LICENSE
-    cp README.md $doc/share/doc/sshrm/README.md
+    ### Add license file accessible on the doc directory
+    cp LICENSE $doc/share/doc/${pname}/LICENSE
+    cp README.md $doc/share/doc/${pname}/README.md
   '';
-
-  #meta = with stdenv.lib; {
-  #  description = "A tool to remove quickly all keys belonging to the specified host from a known_hosts file.";
-  #  homepage = "https://github.com/aaaaadrien/sshrm";
-  #  license = licenses.gpl3;
-  #  maintainers = with maintainers; [ minegameYTB ];
-  #  platforms = lib.platforms.linux;
-  #};
+  
+  postFixup = ''
+    ### Add runtime path to sshrm tool
+    wrapProgram $out/bin/${pname} \
+      --set PATH ${lib.makeBinPath [ sshUtilsOnly ]}
+  '';
  }
